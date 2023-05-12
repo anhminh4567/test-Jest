ed6fe780d4dca6f9ec8ba847cf10773a645be688
ddwcsanh <ledoducanh03@gmail.com>
2023-05-12 17:25:36 +0700

    5th

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
-export default class Navigation extends Component {
-  render() {
-    return (
-      <div class="header">
-        <nav>
-          <ul class="menu">
-            <li>
-              <a className='active' href='#home'>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#news'>News</a>
-            </li>
-            <li>
-              <a href='#about'>About</a>
-            </li>
-            <li>
-              <a href='#contact '>Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+import React from "react";
+
+function Navigation() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a className='active' href='#home'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href='#news'>News</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#contact '>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
+
+export default Navigation;
